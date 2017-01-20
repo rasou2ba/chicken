2e417fe08d065807f52bc90fbb59b06624c0a2e8
isac <isac.lee@utexas.edu>
2017-01-20 14:33:48 -0500

    error

--- alignment/run_align.sh
+++ alignment/run_align.sh
@@ -19,7 +19,7 @@ do
 	#sbatch bismark_cat.sh $samp
     fi
     
-    if [ 0 -eq 1 ]; thenz
+    if [ 0 -eq 1 ]; then
 	    qsub -pe orte 16 -hold_jid bismarkcat_${samp} -N bismarkextract_${samp} -cwd bismark_extract.sh $samp
     fi
 
