06536a770a5a49aefde9289bc97c8c07ca0950b7
isac <isac.lee@utexas.edu>
2017-01-20 22:11:14 -0500

    testing html

--- alignment/bismark_extract_marcc.sh
+++ alignment/bismark_extract_marcc.sh
@@ -10,17 +10,19 @@
 #SBATCH --mail-type=end
 #SBATCH --mail-user=ilee29@jhu.edu
 
+wdir=$PWD
 outdir=/scratch/groups/wtimp1/170119_chicken/aligned/${1}
 bismarkpath=/home-2/ilee29@jhu.edu/Code/bismark_v0.17.0
 refpath=/scratch/groups/wtimp1/Reference/chicken/galGal5
 
-${bismarkpath}/bismark_methylation_extractor -p --multicore 8 --gzip \
+#${bismarkpath}/bismark_methylation_extractor -p --multicore 8 --gzip \
     --genome_folder ${refpath} \
     ${outdir}/${1}.full.bam -o ${outdir} --no_header
 
-${bismarkpath}/bismark2bedGraph --dir ${outdir}/ --counts -o ${1}.full.bedGraph CpG*${1}*.txt.gz 
+#${bismarkpath}/bismark2bedGraph --dir ${outdir}/ --counts -o ${1}.full.bedGraph CpG*${1}*.txt.gz 
 
 cd $outdir
 
-${bismarkpath}/bismark2report
+${bismarkpath}/bismark2report 
 
+cd $wdir
