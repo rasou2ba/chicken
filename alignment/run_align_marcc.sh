ae71565e834a446e61267b6cdb681af8f191a190
isac <isac.lee@utexas.edu>
2017-01-23 15:36:35 -0500

    cat

--- alignment/run_align_marcc.sh
+++ alignment/run_align_marcc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-outdir=/scratch/groups/wtimp1/170119_chicken/aligned/clnref
+outdir=/scratch/groups/wtimp1/170119_chicken/aligned/
 
 for samp in ACTTGA AGTCAA AGTTCC ATGTCA CAGATC CCGTCC CTTGTA GATCAG GGCTAC GTCCGC GTGAAA TAGCTT
 do
@@ -14,11 +14,11 @@ do
 	fi
     done
     
-    if [[  1 -eq 0 ]]; then
+    if [[  0 -eq 0 ]]; then
 	sbatch bismark_cat_marcc.sh ${samp} ${outdir}/${samp}
     fi
     
-    if [ 0 -eq 0 ]; then
+    if [ 1 -eq 0 ]; then
 	sbatch bismark_extract_marcc.sh ${samp} ${outdir}/${samp}
     fi
 
